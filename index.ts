b4b7b862de328c354bfb001fa2c5e50ab37344fa
Rutam Bhagat <rutambhagat@gmail.com>
2026-05-27 17:14:48 +0530

    feat: add config

--- index.ts
+++ index.ts
@@ -4,4 +4,12 @@ import Speaker from "speaker";
 
 import { env } from "./env/server.ts";
 
+const model = "gemini-3.1-flash-live-preview";
+const config = {
+  responseModalities: [Modality.AUDIO],
+  systemInstruction: "You are a helpful and friendly AI assistant.",
+  outputAudioTranscription: {},
+  inputAudioTranscription: {},
+};
+
 const ai = new GoogleGenAI({ apiKey: env.GEMINI_API_KEY });
